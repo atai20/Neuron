903ae78c0cf48366aa490553c4eda654a06bddf4
atai20 <37374053+atai20@users.noreply.github.com>
2022-07-25 01:06:06 +0200

    ert
    ert

--- ConsoleApp3/Neuron.cs
+++ ConsoleApp3/Neuron.cs
@@ -6,83 +6,113 @@ namespace Neuron
 {
     class Neuron
     {
-        public List<double>[] weights;
-        public List<double> inp_weights;
-        public List<double>[] new_hid_layers;
-        public double[] new_inp_layers;
+        List<List<double>[]> weights = new List<List<double>[]>();
+        public List<double>[] hid_layers;
 
-        public void Study(string param)
+
+        public void Study(double[] expect, double rate)
         {
-            Console.WriteLine("Здесь пока ничего нет");
+            for (int i1 = weights.Count - 1; i1 == 0; i1--)
+            {
+
+                for (int i2 = 0; i2 < weights[i1 - 1].Length; i2++)
+                {
+
+                    hid_layers[i1 - 1][i2] = 0;
+
+                    for (int i3 = 0; i3 < weights[i1 - 1][i2].Count; i3++)
+                    {
+                        double[] error = expect;
+
+                        for (int i4 = weights.Count - 1; i4 == 0; i4--)
+                        {
+                            double weights_delta = error[i4] * hid_layers[i1][i3];
+                            weights[i1][i2][i3] -= weights_delta * rate;
+                        }
+
+                        hid_layers[i1 - 1][i2] += hid_layers[i1][i3] * weights[i1][i2][i3];
+
+                    }
+
+                }
+
+            }
+        }
+        public void Compile()
+        {
+
+            for (int i1 = 1; i1 <= weights.Count; i1++)
+            {
+
+                for (int i2 = 0; i2 < weights[i1 - 1].Length; i2++)
+                {
+
+                    for (int i3 = 0; i3 < weights[i1 - 1][i2].Count; i3++)
+                    {
+                        hid_layers[i1][i3] = 0;
+                    }
+                    for (int i3 = 0; i3 < weights[i1 - 1][i2].Count; i3++)
+                    {
+
+                        hid_layers[i1][i3] += hid_layers[i1 - 1][i2] * weights[i1 - 1][i2][i3];
+
+                    }
+
+                }
+
+            }
+
 
         }
-        public void Compile(string param)
+
+        public void Create(List<double>[] pl_hid_layers)
         {
 
-            for (int i = 0; i < new_hid_layers.Length; i++)
+            hid_layers = pl_hid_layers;
+
+            for (int i2 = 0; i2 < hid_layers.Length; i2++)
             {
-                for (int i2 = 0; i2 < new_hid_layers[i].Count; i2++)
+
+                if (i2 <= hid_layers[i2].Count)
                 {
-                
-                    for (int i3 = 0; i3 < weights[i+1].Count; i3++)
+                    List<double>[] h_lay_w = new List<double>[hid_layers[i2].Count];
+                    for (int i3 = 0; i3 < hid_layers[i2].Count; i3++)
                     {
-                        new_hid_layers[i][i2] += weights[i+1][i3] * new_hid_layers[i][i2];
+                        List<double> h_lay_w_in = new List<double>();
+                        for (int i4 = 0; i4 < hid_layers[i2 + 1].Count; i4++)
+                        {
+                            Random rand2 = new Random();
+                            double rand_int2 = rand2.NextDouble();
+                            h_lay_w_in.Add(rand_int2);
+                        }
+                        h_lay_w[i3] = h_lay_w_in;
+
+
                     }
+                    weights.Add(h_lay_w);
                 }
             }
 
 
-        }
-        
-        public void Create(double[] inp_layers, List<double>[] hid_layers)
-        {
-            List<List<double>[]> weights = new List<List<double>[]>();
-            inp_weights = new List<double>();
-            List<double>[] h_lay_w = new List<double>[0];
-
-
-
-
-            for (int i = 0; i < inp_layers.Length * hid_layers[0].Count; i++)
-            {
-
-                Random rand1 = new Random();
-                double rand_int1 = rand1.NextDouble();
-                inp_weights.Add(rand_int1);
-
-            }
-            
-            for (int i2 = 1; i2 < hid_layers.Length; i2++)
-            {
-                h_lay_w = new List<double>[hid_layers[i2 + 1].Count];
-                for (int i3 = 0; i3 < hid_layers[i2].Count; i3++)
-                {
-                    for (int i4 = 0; i4 < hid_layers[i2+1].Count; i4++)
-                    {
-                        Random rand2 = new Random();
-                        double rand_int2 = rand2.NextDouble();
-                        
-                    }
-                    
-
-                }
-                weights.Add(h_lay_w);
-            }
-            new_hid_layers = hid_layers;
-            new_inp_layers = inp_layers;
         }
         public string return_weights()
         {
             string result = "";
-            for (int i1 = 0; i1 < weights.Length; i1++)
+            for (int i1 = 0; i1 < weights.Count; i1++)
             {
-
-                for (int i2 = 0; i2 < weights[i1].Count; i2++)
+                result = result + "[ " + i1 + "; ";
+                for (int i2 = 0; i2 < weights[i1].Length; i2++)
                 {
-                    result = result + "[" + i1 + ", " + weights[i1][i2] + "]; ";
+                    result = result + "[ " + i2 + "; ";
+                    for (int i3 = 0; i3 < weights[i1][i2].Count; i3++)
+                    {
+                        result = result + " " + weights[i1][i2][i3] + "; ";
+                    }
+                    result = result + " ]; ";
                 }
+                result = result + "]; ";
             }
-            return result+"   "+inp_weights.Count;
+            return result + "   " + weights.Count;
         }
 
 
--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,13 +8,24 @@ namespace Neuron
         {
             Neuron ax = new Neuron();
 
-            double[] input = new double[3] {2, 2, 3};
-            List<double>[] hidden_layers = new List<double>[3] { new List<double>{ 0.2, 0.2 }, new List<double> { 0.2, 0.2 }, new List<double> { 0.2, 0.2 } };
-            
+            double[] input = new double[3] { 0.2, 0.4, 0.2 };
+            double[] expect = new double[2] { 0.26, 0.5 };
 
-            ax.Create(input, hidden_layers);
-            ax.Study("sigm");
+            List<double>[] hidden_layers = new List<double>[4] { new List<double> { 0.01, 0.11, 0.2 }, new List<double> { 0.1, 0.2, 0.3 }, new List<double> { 0.4, 0.5 }, new List<double> { 0.6, 0.7 } };
+
+
+            ax.Create(hidden_layers);
             Console.WriteLine(ax.return_weights());
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            ax.Compile();
+            ax.Study(expect);
+
+
+
         }
     }
 }
